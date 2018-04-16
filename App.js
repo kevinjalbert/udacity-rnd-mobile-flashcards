c621b128940d6c8ebc8b1705b256741a138bbe02
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2018-04-15 21:16:58 -0400

    Add eslint/prettier, make rule changes

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation'
-import { FontAwesome } from '@expo/vector-icons'
+import { StyleSheet, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import { FontAwesome } from '@expo/vector-icons';
 
-import { AddDeckScreenContainer } from './containers/AddDeckScreenContainer'
-import { DecksScreenContainer } from './containers/DecksScreenContainer'
+import { AddDeckScreenContainer } from './containers/AddDeckScreenContainer';
+import { DecksScreenContainer } from './containers/DecksScreenContainer';
 
+/* eslint-disable react/prop-types */
 const Tabs = TabNavigator({
   Decks: {
     screen: DecksScreenContainer,
     navigationOptions: {
       tabBarLabel: 'Decks',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='book' size={30} color={tintColor} />
-    }
+      tabBarIcon: ({ tintColor }) => <FontAwesome name="book" size={30} color={tintColor} />,
+    },
   },
   AddDeck: {
     screen: AddDeckScreenContainer,
     navigationOptions: {
       tabBarLabel: 'Add Deck',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    }
+      tabBarIcon: ({ tintColor }) => <FontAwesome name="plus-square" size={30} color={tintColor} />,
+    },
   },
 });
+/* eslint-enable react/prop-types */
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default class App extends React.Component {
   render() {
@@ -33,9 +40,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
